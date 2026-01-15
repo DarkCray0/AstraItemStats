fad15306b8e51742be09d8e2cfc8443c7d1e9408
DarkCray0 <156240056+DarkCray0@users.noreply.github.com>
2026-01-15 12:47:59 -0330

    Base

--- src/main/java/me/darkcray_/astraItemStats/AstraItemStats.java
+++ src/main/java/me/darkcray_/astraItemStats/AstraItemStats.java
@@ -1,13 +1,16 @@
 package me.darkcray_.astraItemStats;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AstraItemStats extends JavaPlugin {
 
+    @Getter
+    private static AstraItemStats instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
     }
 
     @Override
